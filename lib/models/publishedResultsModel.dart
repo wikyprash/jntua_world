c3ca6794660f63287dedd613f7a03c16a6f50da1
wikyprash <wikyprash@gmail.com>
2020-08-29 15:47:38 +0530

    published result
    publihed results  added in dashboard and made some change in api i.e changed routenames,...

---
+++ lib/models/publishedResultsModel.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final publishedResultsModel = publishedResultsModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<PublishedResultsModel> publishedResultsModelFromJson(String str) => List<PublishedResultsModel>.from(json.decode(str).map((x) => PublishedResultsModel.fromJson(x)));
+
+String publishedResultsModelToJson(List<PublishedResultsModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class PublishedResultsModel {
+    PublishedResultsModel({
+        this.publishdDate,
+        this.title,
+        this.url,
+    });
+
+    DateTime publishdDate;
+    String title;
+    String url;
+
+    factory PublishedResultsModel.fromJson(Map<String, dynamic> json) => PublishedResultsModel(
+        publishdDate: DateTime.parse(json["publishd_date"]),
+        title: json["title"],
+        url: json["url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "publishd_date": "${publishdDate.year.toString().padLeft(4, '0')}-${publishdDate.month.toString().padLeft(2, '0')}-${publishdDate.day.toString().padLeft(2, '0')}",
+        "title": title,
+        "url": url,
+    };
+}
